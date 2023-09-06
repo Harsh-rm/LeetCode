7ad5bd1a63284a807d52e72b589a5b79e25bb179
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2023-09-05 19:47:15 -0700

    Time: 1 ms (71.08%), Space: 41.9 MB (46.45%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<>();
+
+        if (nums == null || nums.length == 0) return result;
+        
+        helper(nums, 0, new ArrayList<>());
+
+        return result;
+    }
+
+    private void helper(int[] nums, int index, List<Integer> path) {
+        //base
+        result.add(new ArrayList<>(path));
+        //logic
+        for(int i = index; i < nums.length; i++) {
+            //action
+            path.add(nums[i]);
+            //recursion
+            helper(nums, i + 1, path);
+            //backtracking
+            path.remove(path.size() - 1);
+        }
+    }
+}
