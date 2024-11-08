f6885a220b0bf98bf5f9bd8b9c5b64f4fd95ddce
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-11-08 12:05:08 -0800

    Time: 0 ms (100%), Space: 41.2 MB (80.64%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -3,15 +3,24 @@ class Solution {
         if (citations == null || citations.length == 0) return 0;
 
         int n = citations.length;
-        Arrays.sort(citations);
+        int[] nums = new int[n + 1];
 
         for (int i = 0; i < n; i++) {
-            int diff = n - i;
-            if (citations[i] >= diff) {
-                return diff;
+            if (citations[i] > n) {
+                nums[n]++;
+            } else {
+                nums[citations[i]]++;
             }
         }
+        int rsum = 0;
 
-        return 0;
+        for (int i = n; i >= 0; i--) {
+            rsum += nums[i];
+            if (rsum >= i) {
+                return i;
+            }            
+        }
+
+        return rsum;
     }
 }
