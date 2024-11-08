0c36a6ba172481ef1365600ef7ae962ec51ee31c
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-11-07 22:48:37 -0800

    Time: 1 ms (91.36%), Space: 44.6 MB (93.84%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -19,28 +19,22 @@ class Solution {
         result = new ArrayList<>();
         if (root == null) return result;
 
-        //BFS based solution
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        //DFS Based solution
+        dfs(root, 0);
 
-        while(!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> list = new ArrayList<>();
-             
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-                list.add(curr.val);
-                if (curr.left != null) {
-                    q.add(curr.left);
-                }
-                if (curr.right != null) {
-                    q.add(curr.right);
-                }
-            }
+        return result;
+    }
 
-            result.add(list);
+    private void dfs(TreeNode root, int level) {
+        //Base
+        if (root == null) return;
+        //Logic
+        if (result.size() == level) {
+            result.add(new ArrayList<>());
         }
+        result.get(level).add(root.val);
 
-        return result;        
-    } 
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
 }
