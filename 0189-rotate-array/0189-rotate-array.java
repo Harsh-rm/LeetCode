db3ce91d50b0cfdb1c666f29b921c62b56934a2e
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-11-13 13:18:51 -0800

    Time: 1 ms (48.56%), Space: 57.5 MB (23.15%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -6,11 +6,9 @@ class Solution {
         
         if (k > n) k = k % n;
 
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
         reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
-
-        return;
     }
 
     private void reverse(int[] nums, int start, int end) {
