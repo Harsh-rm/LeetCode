cc931d8a7190bef910bf211fdc22fb9a9d7f384e
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-11-18 12:17:28 -0800

    Time: 23 ms (80.19%), Space: 54.4 MB (99.24%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -3,17 +3,16 @@ class Solution {
         if (nums == null || nums.length == 0) return 0;
 
         int maxLength = 0;
-        int n = nums.length;
         int rSum = 0;
         HashMap<Integer, Integer> rSumtoIndexMap = new HashMap<>();
         
         rSumtoIndexMap.put(rSum, -1);
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
-                rSum += -1;
+                rSum--;
             } else {
-                rSum += 1;
+                rSum++;
             }
 
             if (!rSumtoIndexMap.containsKey(rSum)) {
