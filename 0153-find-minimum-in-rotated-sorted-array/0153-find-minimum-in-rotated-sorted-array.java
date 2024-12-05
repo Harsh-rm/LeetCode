c0a7c17b6d4476591d57780e48f068d826330148
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-04 19:14:58 -0800

    Time: 0 ms (100%), Space: 42 MB (49.44%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,27 +1,36 @@
+/*
+    Leetcode problem 153: Find Minimum in Rotated Sorted Array
+    T.C: O(log(n)) :: S.C: O(1)
+
+    Solved using B.S technique -
+    Base case is to check if the element at low < high (the array is sorted)
+    then calculate the mid and check if the mid is the min element
+    if not then the min element lies in the unsorted half of the array
+    assign low and high accordingly and re-iterate
+*/
 class Solution {
     public int findMin(int[] nums) {
-        if(nums == null || nums.length == 0) return -1;
+        if (nums == null || nums.length == 0) return -1;
 
-        int low = 0;
         int n = nums.length;
-        int high = n - 1;
+        int low = 0, high = n - 1;
 
         while (low <= high) {
-          if (nums[low] <= nums[high]) return nums[low];
+            if (nums[low] <= nums[high]) return nums[low];
 
-          int mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
 
-          if ((mid == low || nums[mid - 1] > nums[mid]) && (mid == high || nums[mid + 1] > nums[mid])) {
-            return nums[mid];
-          }
+            if ((mid == low || nums[mid - 1] > nums[mid]) && (mid == high || nums[mid] < nums[mid + 1])) {
+                return nums[mid];
+            } 
 
-          if (nums[low] <= nums[mid]) {
-            low = mid + 1;
-          }
-          else {
-            high = mid - 1;
-          }
+            if (nums[low] <= nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        return -1;
+
+        return Integer.MAX_VALUE;
     }
 }
