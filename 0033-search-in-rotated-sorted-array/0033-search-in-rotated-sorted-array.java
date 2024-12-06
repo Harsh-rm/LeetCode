01262493794e9df16364860f8218b23d545eaa89
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-05 22:17:05 -0800

    Time: 0 ms (100%), Space: 42.1 MB (46.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,3 +1,12 @@
+/*
+    Leetcode problem 33: Search in Rotated Sorted Array
+    T.C: O(log(n)) :: S.C: O(1)
+
+    Solved using B.S technique:
+    Find the sorted half from the mid and check if the target lies in
+    the sorted half else perform binary search on the unsorted half
+    repeat the above steps until the target is found else return -1.
+*/
 class Solution {
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
