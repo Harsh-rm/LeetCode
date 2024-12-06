8a2fc77a0b2e7e8954aab52e435af94c84ae15ac
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-06 14:17:39 -0800

    Time: 0 ms (100%), Space: 41.2 MB (73.22%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,17 +1,31 @@
+/*
+    Leecode Problem 232: Implement Queue using Stacks
+    T.C push(): O(1), T.C pop() & peek(): O(1) Amortized - O(n) Worst case, T.C empty(): O(1)
+    S.C: O(n) - two stacks i.e O(n) + O(n) = O(2n) - 2 is constant
+
+    Solved using 2 Stacks: refer comments for Impl.
+*/
+
 class MyQueue {
+    //Declare the 2 stacks to perform queue operations
     Stack<Integer> in;
     Stack<Integer> out;
 
     public MyQueue() {
+        //Initialize the stacks in the constructor
         in = new Stack<>();
         out = new Stack<>();
     }
     
     public void push(int x) {
+        //Directly push to in stack
         in.push(x);
     }
     
     public int pop() {
+        //Check is out stack is empty, if yes transfer all the elements 
+        //from in stack to out, then pop elements one by one.
+        //Else pop directly from out stack.
         peek();
         return out.pop();
     }
@@ -29,6 +43,7 @@ class MyQueue {
     }
     
     public boolean empty() {
+        //Check if both the stacks are empty and return the truth value
         if (out.isEmpty() && in.isEmpty()) {
             return true;
         }
