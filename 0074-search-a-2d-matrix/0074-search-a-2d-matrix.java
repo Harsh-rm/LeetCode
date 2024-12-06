be7fc278a293f9d355ad9291a3e2a9c917eff9b8
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-05 22:13:35 -0800

    Time: 0 ms (100%), Space: 42.4 MB (14.43%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,10 +1,21 @@
+/*
+    Leetcode problem 74: Search a 2D Matrix
+    T.C: O(log(m * n)) :: S.C: O(1)
+
+    Solved using B.S technique:
+    As the elements are sorted from left to right and following to the next row.
+    Each row indvidually can be considered as a linearly sorted array an we can
+    perform binary search to get the target element and return the truth value.
+*/
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0) return false;
+        boolean result = false;
+
+        if (matrix == null || matrix.length == 0) return result;
 
         int n = matrix.length;
-        int m = matrix[0].length;
-        boolean result = false;
+        int m = matrix[0].length;        
 
         for (int i = 0; i < n; i++) {
             result = binarySearch(matrix[i], 0, m - 1, target);
