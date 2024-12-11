bfb5e55144781ef6ca60beb32cb6aac20a84f53b
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-11 13:08:29 -0800

    Time: 26 ms (33.88%), Space: 47 MB (11.86%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -3,29 +3,20 @@ class Solution {
         if (nums == null || nums.length == 0) return 0;
 
         int count = 0;
-        ArrayList<int[]> result = new ArrayList<>();
-
         int rSum = 0;
-        HashMap<Integer, ArrayList<Integer>> rSumMap = new HashMap<>();
+        HashMap<Integer, Integer> rSumMap = new HashMap<>();
 
-        rSumMap.put(rSum, new ArrayList<>());
-        rSumMap.get(rSum).add(-1);
+        rSumMap.put(rSum, 1);
+ 
 
         for (int i = 0; i < nums.length; i++) {
             rSum += nums[i];
 
-            // Check if there exists a runningSum such that runningSum - k exists in the map
-            int diff = rSum - k;
-            if (rSumMap.containsKey(diff)) {
-                for (int startIdx : rSumMap.get(diff)) {
-                    //result.add(Arrays.copyOfRange(nums, startIdx + 1, i + 1));
-                    count++;
-                }
+            if (rSumMap.containsKey(rSum - k)) {
+                count += rSumMap.get(rSum - k);
             }
 
-            // Add the current running sum to the map
-            rSumMap.putIfAbsent(rSum, new ArrayList<>());
-            rSumMap.get(rSum).add(i);
+            rSumMap.put(rSum, rSumMap.getOrDefault(rSum, 0) + 1);
         }
 
         return count;
