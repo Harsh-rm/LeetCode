7dd31d326fa2610247d188e7848b9820ce4001fe
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-10 19:37:10 -0800

    Time: 1 ms (81.71%), Space: 41.6 MB (26.8%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,33 +1,32 @@
 class Solution {
-
     public boolean wordPattern(String pattern, String s) {
-        String[] strArray = s.split(" ");
+        if (pattern == null || s == null) return false;
 
-        if (pattern.length() != strArray.length) return false;
+        String[] sArray = s.split(" ");
+
+        if (pattern.length() != sArray.length) return false;
 
         HashMap<Character, String> charToStringMap = new HashMap<>();
-        HashSet<String> stringMappedTochar = new HashSet<>();
+        HashSet<String> mappedStringSet = new HashSet<>();
 
         for (int i = 0; i < pattern.length(); i++) {
-            String str = strArray[i];
             char c = pattern.charAt(i);
 
             if (!charToStringMap.containsKey(c)) {
-                charToStringMap.put(c, str);
+                charToStringMap.put(c, sArray[i]);
 
-                if(!stringMappedTochar.contains(str)) {
-                    stringMappedTochar.add(str);
+                if (!mappedStringSet.contains(sArray[i])){
+                    mappedStringSet.add(sArray[i]);
                 } else {
                     return false;
                 }
             } else {
-                if (!str.equals(charToStringMap.get(c))) {
+                if (!charToStringMap.get(c).equals(sArray[i])) {
                     return false;
-                }                
+                }
             }
         }
 
         return true;
     }
-
 }
