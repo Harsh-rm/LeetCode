a416cef4077d67cbeb307709866f9aa82932afe2
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-11 11:51:08 -0800

    Time: 2737 ms (5%), Space: 46.1 MB (78.05%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -2,21 +2,33 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         if (nums == null || nums.length == 0) return 0;
 
-        HashMap<Integer, Integer> rsumfreqMap = new HashMap<>();
-        int rSum = 0;
         int count = 0;
-        rsumfreqMap.put(rSum, 1);
+        ArrayList<int[]> result = new ArrayList<>();
+        ArrayList<Integer> rSum = new ArrayList<>();
+        rSum.add(0);
 
         for (int i = 0; i < nums.length; i++) {
-            rSum += nums[i];
-
-            if (rsumfreqMap.containsKey(rSum - k)) {
-                count += rsumfreqMap.get(rSum - k);
-            }
-
-            rsumfreqMap.put(rSum, rsumfreqMap.getOrDefault(rSum, 0) + 1);
+            rSum.add(rSum.get(i) + nums[i]);
         }
+        
+        //System.out.println(rSum);
 
+        for (int i = nums.length; i >= 0; i--) {
+            int diff = rSum.get(i) - k;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (rSum.get(j) == diff) {
+                    //int subArray = Arrays.copyOfRange(nums, j , i);                   
+                    //result.add(subArray);
+                    count++;                    
+                }
+            }
+        }
+    /*
+        for (int[] subArray : result) {
+            System.out.println(Arrays.toString(subArray));
+        }
+    */
         return count;
     }
 }
