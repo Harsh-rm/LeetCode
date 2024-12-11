a1bd74efa609eddb970def1e6acf64eab1531332
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-10 18:52:20 -0800

    Time: 6 ms (93.13%), Space: 42.6 MB (32.89%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,30 +1,28 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() == 0 || t.length() == 0 || s.length() != t.length()) return false;
+        if (s.length() != t.length()) return false;
 
-        HashMap<Character, Character> checkIso = new HashMap<>();
-        HashSet<Character> charMapped = new HashSet<>();
+        char[] sArray = new char[96]; //store sChar to tChar mapping
+        char[] tArray = new char[96]; //store tChar to sChar mapping
 
         for (int i = 0; i < s.length(); i++) {
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
 
-            if(!checkIso.containsKey(sChar)){
-
-                if (!charMapped.contains(tChar)) {
-                    charMapped.add(tChar);
-                    checkIso.put(sChar, tChar);
-                } else {
-                    return false;
-                }
-
+            if (sArray[sChar - ' '] == '\u0000') {
+                sArray[sChar - ' '] = tChar;
             } else {
-                // Challenge lies in this condition - if the char is already mapped then we need to check if the mapping element is the same
-                //As we are using Hashset based solution we need to check if current mapping element is in the hashset
-                if (!charMapped.contains(tChar) || tChar != checkIso.get(sChar)) { 
+                if (sArray[sChar - ' '] != tChar) {
                     return false;
                 }
+            }
 
+            if (tArray[tChar - ' '] == '\u0000') {
+                tArray[tChar - ' '] = sChar;
+            } else {
+                if (tArray[tChar - ' '] != sChar) {
+                    return false;
+                }
             }
         }
 
