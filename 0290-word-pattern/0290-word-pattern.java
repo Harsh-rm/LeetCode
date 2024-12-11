3b634865a314a846b56bd8266430da8505bfcd21
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-10 19:51:50 -0800

    Time: 1 ms (81.71%), Space: 41.3 MB (56.97%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,3 +1,13 @@
+/*
+    Leetcode problem 290: Word Pattern
+    T.C: O(n) - length of the pattern String
+    S.C: O(n) - O(n + n) = O(2n) (HashMap and HashSet of size n)
+
+    Solved using a HashMap and a HashSet similar to Isomorphic strings
+    Paired each character in the pattern string to the split(" ") string s in the HashMap
+    and added the mapped string s to the HashSet. Comments added for checks in solution.
+*/
+
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         if (pattern == null || s == null) return false;
@@ -12,17 +22,18 @@ class Solution {
         for (int i = 0; i < pattern.length(); i++) {
             char c = pattern.charAt(i);
             String str = sArray[i];
-
+            //Check if the Map contains the key for char c
             if (!charToStringMap.containsKey(c)) {
                 charToStringMap.put(c, str);
-
-                if (!mappedStringSet.contains(str)) {
+                //Insert the char key and string value
+                if (!mappedStringSet.contains(str)) { 
                     mappedStringSet.add(str);
-                } else {
+                } else { //If the string was previously mapped return false
                     return false;
                 }
             } else {
-                if (!charToStringMap.get(c).equals(str)) {
+                //If the we are checking a key that already exist in map then 
+                if (!charToStringMap.get(c).equals(str)) { // it should match the current string value
                     return false;
                 }
             }
