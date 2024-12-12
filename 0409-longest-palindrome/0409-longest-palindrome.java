8c0079664ba0dbe25709c5cb8575faaa3b19bd01
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-11 17:13:30 -0800

    Time: 8 ms (27.64%), Space: 42.1 MB (24%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -2,24 +2,35 @@ class Solution {
     public int longestPalindrome(String s) {
         if (s == null || s.length() == 0) return 0;
 
-        int longestResult = 0;
-        HashSet<Character> setChecksPairs = new HashSet<>();        
+        HashMap<Character, Integer> charOccMap = new HashMap<>();
+        int longest = 0;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            if (setChecksPairs.contains(c)) {
-                longestResult += 2;
-                setChecksPairs.remove(c);
+            charOccMap.put(c, charOccMap.getOrDefault(c, 0) + 1);
+        }
+
+        boolean flag = true;
+
+        for (char key: charOccMap.keySet()) {
+            if (charOccMap.get(key) % 2 == 0) {
+                longest += charOccMap.get(key);
+            } else if (charOccMap.get(key) > 2) {
+                if (flag) {
+                    longest += charOccMap.get(key);
+                    flag = false;
+                } else {
+                    longest += charOccMap.get(key) - 1;                    
+                }
             } else {
-                setChecksPairs.add(c);
+                if (flag) {
+                    longest++;
+                    flag = false;
+                }
             }
         }
 
-        if (setChecksPairs.size() > 0) {
-            longestResult += 1;
-        }
-        
-        return longestResult;
+        return longest;
     }
 }
