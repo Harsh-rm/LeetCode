f4c4b86b246a004f689345749d2ad9a3f991b4cf
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2024-12-11 23:50:54 -0800

    Time: 35 ms (5.58%), Space: 55.1 MB (57.47%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,27 +1,37 @@
+/*
+    Leetcode problem 525: Contiguous Array
+    T.C: O(n) :: S.C: O(n)
+
+    Solved using running sum pattern (i.e z = x - y, where z is the difference
+    between current rSum and the index at which you intend to find your target sum)
+    For this problem if rSum - HashMap.get(rSum) == 0 then a balanced number of
+    contiguous 0s and 1s exist in the subArray
+*/
+
 class Solution {
     public int findMaxLength(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
 
-        int maxLength = 0;
+        int resultLen = 0;
         int rSum = 0;
-        HashMap<Integer, Integer> rSumtoIndexMap = new HashMap<>();
+        HashMap<Integer, Integer> rSumIndexMap = new HashMap<>();
         
-        rSumtoIndexMap.put(rSum, -1);
+        rSumIndexMap.put(rSum, -1);
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
-                rSum--;
+                rSum--;                
             } else {
-                rSum++;
+                rSum++; 
             }
 
-            if (!rSumtoIndexMap.containsKey(rSum)) {
-                rSumtoIndexMap.put(rSum, i);
-            } else {
-                maxLength = Math.max(maxLength, i - rSumtoIndexMap.get(rSum));
+            rSumIndexMap.putIfAbsent(rSum, i);
+
+            if (rSumIndexMap.containsKey(rSum)){
+                resultLen = Math.max(resultLen, i - rSumIndexMap.get(rSum));
             }
         }
 
-        return maxLength;
+        return resultLen;
     }
 }
