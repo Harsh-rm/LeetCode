906538323cd1a96907c2cbc33d9270edf592f93f
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-10 13:19:03 -0800

    Time: 1 ms (5.38%), Space: 42.5 MB (47.23%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,23 +8,51 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*
+    Leetcode problem 206: Reverse a Linked List
+    T.C: O(n) :: S.C: O(1)
+
+    Solved using two pointers: 
+*/
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null) return null;
-        if (head.next == null) return head;
+        if (head == null) return head;
 
-        ListNode slow = null;
-        ListNode curr = head;
-        ListNode fast = head.next;
+        ListNode dummy = new ListNode(Integer.MIN_VALUE, head);
+        Stack<ListNode> s = new Stack<>();
+        ListNode curr = dummy.next;
 
-        while(fast != null) {
-            curr.next = slow;
-            slow = curr;
-            curr = fast;
-            fast = fast.next;
+        while (curr != null) {
+            s.push(curr);
+            curr = curr.next;
         }
-        curr.next = slow;
-        
-        return curr;
+
+        dummy.next = s.pop();
+        curr = dummy.next;
+
+        while (!s.isEmpty()) {
+            curr.next = s.pop();
+            curr = curr.next;
+        }
+
+        curr.next = null;
+/*
+        ListNode curr = dummy;
+        ListNode ahead = dummy.next;
+
+        while (ahead != null && ahead.next != null) {
+            ListNode prev = dummy.next;
+            dummy.next = ahead;
+            ahead = ahead.next;
+            dummy.next.next = prev;
+            curr.next = ahead;
+            curr = ahead;
+
+            System.out.println(dummy.next.val);
+        }
+*/
+        return dummy.next;
     }
 }
