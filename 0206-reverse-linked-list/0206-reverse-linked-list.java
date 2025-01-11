1490f356357badf6db8dab8663bf8a8efcfac05f
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-11 12:58:13 -0800

    Time: 0 ms (100%), Space: 42.6 MB (47.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -13,7 +13,15 @@
     Leetcode problem 206: Reverse a Linked List
     T.C: O(n) :: S.C: O(1)
 
-    Solved using three pointers: Previous pointing to null, Current pointing to 
+    I solved this problem in an unorthodox approach, unlike the regular way of reversing 
+    the pointers for each node to its previous.
+
+    Solved using three pointers: Previous pointing to head, Current pointing to the head
+    and front (as next is used as a reference in the class ListNode) point to the node next
+    to head. Now, we make the dummy point to the front node and then move front to the next node, 
+    typically we are traversing through the linked list and then sending each of the nodes to the
+    head and making sure the linked list has the proper chain, where all the nodes are connected w.r.t
+    to the original links. Refer the comments in the code to understand better.
 */
 
 class Solution {
@@ -24,17 +32,24 @@ class Solution {
 
         ListNode prev = head;
         ListNode curr = head;
-        ListNode fast = head.next;
+        ListNode front = head.next;
 
-        while (fast != null) {
-            dummy.next = fast;
-            fast = fast.next;
-            curr.next = fast;
+        while (front != null) {
+            //Bring the node in the front to the head
+            dummy.next = front;
+            //Now move front to the next position in LL
+            front = front.next;
+            //Organize the LL by making sure the initial head is always connected to the front node
+            curr.next = front;
+            //Now connect the current head to the previous head
             dummy.next.next = prev;
+            //Make the current head as the previous head of the LL
             prev = dummy.next;
         }
-        curr.next = fast;
+        //To avoid LL cyle make sure the initial head points to null
+        curr.next = front;
 
+        //Return the dummy's next (which is the new head)
         return dummy.next;
     }
 }
