2c1b9deceb3887bae7cec69c9c828e432b980dce
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-14 19:33:01 -0800

    Time: 2 ms (86.93%), Space: 48.1 MB (76.39%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -8,47 +8,66 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*
+    Leetcode problem 146: Reorder List
+    T.C: O(n) :: S.C: O(1) - Amortized T.C
+
+    Solved using two pointers: fast and slow, Position the slow pointer to the middle of the linked List.
+    Split the Linked list to two halves. Reverse the second half of Linked list (slow.next - write a 
+    helper method). Re-order the Linked list from the split halves (i.e point the head's next to the head
+    of the reversed list and then this next to node back to the initial next node in the LL). Congrats problem solved!
+*/
+
 class Solution {
+
     public void reorderList(ListNode head) {
-        if (head == null) return;
+        if (head == null || head.next == null) return;
 
         ListNode fast = head;
-        ListNode curr = head;
         ListNode slow = head;
 
-        while(fast.next != null && fast.next.next != null) {
+        //Position the slow pointer to the middle of the linked List
+        while (fast.next != null && fast.next.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
 
-        curr = slow.next;
+        //Split the Linked list to two halves
+        fast = slow.next;
         slow.next = null;
-        slow = slow.next;
-        if (curr != null) { 
-            fast = curr.next;
-            while(fast != null) {
-                curr.next = slow;
-                slow = curr;
-                curr = fast;
-                fast = fast.next;
-            }
-            curr.next = slow;
-        }
-        // slow = head;
-        // while(slow != null) {
-        //     System.out.println(slow.val);
-        //     slow = slow.next;
-        // }
 
+        //Reverse the second half of Linked list
+        fast = reverseList(fast);
+
+        //Re-order the Linked list from the split halves
         slow = head;
-        fast = head.next;
-        while(curr != null) {
-            slow.next = curr;
-            curr = curr.next;
-            // slow = slow.next;
-            slow.next.next = fast;
-            slow = fast;
-            if (fast !=  null) fast = fast.next;
+        ListNode temp = slow.next;
+
+        while (fast != null) {
+            slow.next = fast;
+            fast = fast.next;
+            slow.next.next = temp;
+            slow = temp;
+            if (temp != null) temp = temp.next;
         }
+    }
+
+    private ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode prev = null;
+        ListNode fast = head.next;
+        ListNode curr = head;
+
+        while(fast != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;
+        }
+        curr.next = prev;
+
+        return curr;
     }
 }
