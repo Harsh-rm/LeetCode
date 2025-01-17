16ab846ced436fcc788dfbe303a6203c3d1dcf44
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-17 15:08:48 -0800

    Time: 0 ms (100%), Space: 41.6 MB (97.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,3 +1,13 @@
+/*
+    Leetcode problem 27: Remove Element
+    T.C: O(n) :: S.C: O(1)
+
+    Solved using Two pointers: Left and Right. Left starts from index 0 and right starts from index n - 1.
+    If the element at index left is equal to k then swap it with the element at right and move right towards the left,
+    else move the left forward. If left and right cross each, then break the loop and return the index of the left pointer.
+    As the elements until the left pointer have been swapped and they are not equal to k.
+*/
+
 class Solution {
     public int removeElement(int[] nums, int val) {
         if (nums == null) return 0;
