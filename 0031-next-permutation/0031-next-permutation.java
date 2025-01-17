c92531144d23a425d683f9d633260102605e7e08
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-16 19:51:15 -0800

    Time: 1 ms (16.01%), Space: 43.5 MB (10.8%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,19 +1,19 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if (nums == null || nums.length <= 1) return;
+        if (nums == null) return;
 
         int n = nums.length;
         int index = -1;
 
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                index = i - 1;
                 break;
             }
         }
 
         if (index != -1) {
-            for (int i = n - 1; i > index; i--) {
+            for (int i = n - 1; i >= 0; i--) {
                 if (nums[index] < nums[i]) {
                     swap(nums, index, i);
                     break;
@@ -21,20 +21,13 @@ class Solution {
             }
         }
 
-        reverse(nums, index + 1, n -1);
+        Arrays.sort(nums, index + 1, n);
+        
     }
 
-    private void swap(int[] nums, int l, int r) {
-        int temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
-    }
-
-    private void reverse(int[] nums, int l, int r) {
-        while (l <= r) {
-            swap(nums, l, r);
-            l++;
-            r--;
-        }
+    private void swap(int[] nums, int index, int i) {
+        int temp = nums[index];
+        nums[index] = nums[i];
+        nums[i] = temp;
     }
 }
