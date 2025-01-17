1eecbd46cac0f982722354e2fe2329232b565645
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-16 16:38:13 -0800

    Time: 1 ms (90.04%), Space: 45.2 MB (32.51%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,23 +13,36 @@
  *     }
  * }
  */
+
+/*
+    Leetcode problem 102: Binary Tree Level Order Traversal
+    T.C: O(n) :: S.C: O(n) - Space occupied by the Queue
+
+    Solved using BFS: Initialize an empty Queue of TreeNodes with the root (If the root is null, return an empty ArrayList immediately).
+    Then traverse through the Queue by maintianing a size variable for keeping track of each level in the tree (i.e initially size is 1
+    add this element as a new list to the result), go left, and go right, and then add the child if it is not null. Now when you are at 
+    the second level (the size is 2). Repeat this process to retrieve the level order traversal of the Binary tree.
+*/
+
 class Solution {
     List<List<Integer>> result;
+    
     public List<List<Integer>> levelOrder(TreeNode root) {
         result = new ArrayList<>();
+
         if (root == null) return result;
 
-        //BFS based solution
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
 
         while(!q.isEmpty()) {
             int size = q.size();
-            List<Integer> list = new ArrayList<>();
-             
+            List<Integer> li = new ArrayList<>();
+
             for (int i = 0; i < size; i++) {
                 TreeNode curr = q.poll();
-                list.add(curr.val);
+                li.add(curr.val);
+
                 if (curr.left != null) {
                     q.add(curr.left);
                 }
@@ -38,9 +51,9 @@ class Solution {
                 }
             }
 
-            result.add(list);
+            result.add(li);
         }
 
-        return result;        
-    } 
+        return result;
+    }
 }
