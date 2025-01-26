2e57758b0b536ef4547d75761f65aacc74efc63e
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-26 11:45:11 -0800

    Time: 17 ms (65.66%), Space: 48.7 MB (11.1%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -13,29 +13,18 @@
  *     }
  * }
  */
-
-/*
-    Leetcode problem 173: Binary Search Tree Iterator
-    T.C: O(n) :: S.C: O(h) - Amortized S.C
-
-    Solved using DFS Approach: Traverse to the leftmost leaf node of the given root, while pushing
-    each of the TreeNode to a stack. When next() is called, pop the element on top of the stack and 
-    perform the same dfs operation on its right child (do not push if the TreeNode is null). For 
-    the hasNext() method, return the negated true value of isEmpty() for the BSTIterator's stack data structure.
-*/
-
 class BSTIterator {
-    Stack<TreeNode> s;
+        Stack<TreeNode> s;
 
     public BSTIterator(TreeNode root) {
-        s = new Stack<>();
-        dfs(root);   
+        s = new Stack();
+        dfs(root);
     }
     
     public int next() {
-        TreeNode popped = s.pop();
-        dfs(popped.right);
-        return popped.val;
+        TreeNode next = s.pop();
+        dfs(next.right);
+        return next.val;
     }
     
     public boolean hasNext() {
@@ -43,7 +32,7 @@ class BSTIterator {
     }
 
     private void dfs(TreeNode root) {
-        while(root != null) {
+        while (root != null) {
             s.push(root);
             root = root.left;
         }
