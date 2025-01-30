087beae4669717206df4b28d6f65148ea586bff1
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-01-29 18:14:07 -0800

    Time: 185 ms (30.66%), Space: 41.3 MB (94.72%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,46 +1,47 @@
 class Solution {
-
-    int[][] dirs;
+    int[][] dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     int m, n;
 
     public boolean exist(char[][] board, String word) {
-        if (board == null || board.length == 0) return false;
+        if (word == null) return false;
 
         m = board.length;
-        n = board[0].length;        
-        dirs = new int[][] {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; //Dirs => Top, Right, Down, Left
+        n = board[0].length;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (backtrack(board, i, j, word, 0)) {
+                if (dfs(board, word, 0, i, j)) {
                     return true;
                 }
             }
         }
 
-        return false;
+        return false;   
     }
 
-    private boolean backtrack(char[][] board, int r, int c, String word, int index) {
-        //Base
+    private boolean dfs(char[][] board, String word, int index, int i, int j) {
+        //Base case
         if (index == word.length()) return true;
+                    
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == '\u0000') return false;
 
-        if(r < 0 || c < 0 || r == m || c == n || board[r][c] == '#') return false;
-        //Logic
-        if (board[r][c] == word.charAt(index)) {
-            //action
-            char ch = board[r][c];
-            board[r][c] = '#';
-            //recurse
-            for (int[] dir : dirs) {
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-                if (backtrack(board, nr, nc, word, index + 1)) {
+        if (board[i][j] == word.charAt(index)) {
+            //Action
+            char temp = board[i][j];
+            board[i][j] = '\u0000';
+
+            //Recurse
+            for (int[] dir: dirs) {
+                int nr = i + dir[0];
+                int nc = j + dir[1];
+                
+                if (dfs(board, word, index + 1, nr, nc)) {
                     return true;
                 }
             }
-            //backtrack
-            board[r][c] = ch;
+
+            //Backtrack
+            board[i][j] = temp;
         }
 
         return false;
