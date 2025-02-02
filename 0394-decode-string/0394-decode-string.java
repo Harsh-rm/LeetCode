635a7a3c5fc88783005ee427ccbc16b9d9d8c336
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-01 17:39:29 -0800

    Time: 0 ms (100%), Space: 41.4 MB (82.48%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,42 +1,32 @@
 class Solution {
+    int i;
     public String decodeString(String s) {
         if (s == null || s.length() == 0) return "";
-
-        Stack<StringBuilder> strStack = new Stack<>();
-        Stack<Integer> numStack = new Stack<>();
 
         StringBuilder currStr = new StringBuilder();
         int num = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        while (i < s.length()) {
             Character ch = s.charAt(i);
 
             if (Character.isDigit(ch)) {
                 num = num * 10 + Character.getNumericValue(ch);
+                i++;
             }
             else if (ch == '[') {
-                strStack.push(currStr);
-                numStack.push(num);
+                i++;
+                String decoded = decodeString(s);
+                //Pop operation from the inner encoded part that has to be appened to the currStr after multiplying it
+                currStr.append(decoded.repeat(num));
                 num = 0;
-                //currStr.setLength(0); //This modifies the reference of the currStr in the stack and hence does not work
-                currStr = new StringBuilder();
             }
             else if (ch == ']') {
-                int times = numStack.pop();
-            /*  
-                //This Method is more efficient     
-                StringBuilder newStr = new StringBuilder();
-                for (int count = 1; count < times; count++) {                    
-                    newStr.append(currStr);
-                }
-                currStr = strStack.pop().append(newStr);
-            */
-                currStr.append(currStr.toString().repeat(times - 1)); //For Practice
-                StringBuilder str = strStack.pop();
-                currStr.insert(0, str);
+                i++;
+                return currStr.toString();
             }
             else {
                 currStr.append(ch);
+                i++;
             }
         }
 
