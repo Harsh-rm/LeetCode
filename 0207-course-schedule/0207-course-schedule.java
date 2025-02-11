63f6be2ddee7993178aeda3c7b249850f2dcb932
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-10 23:04:28 -0800

    Time: 10 ms (17.42%), Space: 45.3 MB (69.63%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,53 +1,47 @@
 class Solution {
     boolean result;
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         result = false;
 
-        if (numCourses == 0) return result;
+        if (prerequisites.length == 0) return true;
 
-        //These are the courses that are dependant on other courses and have to be taken prior to others 
-        //All sorted in the same list. Note: not index wise sorting rather depandant wise sorting
-        int[] topologicalSort = new int[numCourses];
-
-        //This map contains all the prerequisite keys associated with the dependant courses
-        HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
+        int[] tocomplete = new int[numCourses];
+        HashMap<Integer, List<Integer>> prereqtotakemap = new HashMap<>();
 
         for (int i = 0; i < prerequisites.length; i++) {
-            topologicalSort[prerequisites[i][0]]++;
+            tocomplete[prerequisites[i][0]]++;
 
-            adjacencyList.computeIfAbsent(prerequisites[i][1], key -> new ArrayList<>()).add(prerequisites[i][0]);
+            prereqtotakemap.computeIfAbsent(prerequisites[i][1], key -> new ArrayList<>()).add(prerequisites[i][0]);
         }
 
-        AtomicInteger coursesCompletedAtomic = new AtomicInteger();
+        AtomicInteger count = new AtomicInteger();
         Queue<Integer> bfs = new LinkedList<>();
-
+        
         for (int i = 0; i < numCourses; i++) {
-            if (topologicalSort[i] == 0) {
-                coursesCompletedAtomic.getAndIncrement();
+            if (tocomplete[i] == 0) {
                 bfs.add(i);
+                count.getAndIncrement();
             }
         }
 
         while(!bfs.isEmpty()) {
-            int currCourse = bfs.poll();
+            int curr = bfs.poll();
 
-            Optional.ofNullable(adjacencyList.get(currCourse))
+            Optional.ofNullable(prereqtotakemap.get(curr))
 
-                    .ifPresent(list -> list.forEach(dependantCourse -> {
+                    .ifPresent(list -> list.forEach(course -> {
 
-                        if (--topologicalSort[dependantCourse] == 0) {
-
-                            coursesCompletedAtomic.getAndIncrement();
-
-                            bfs.add(dependantCourse);  
+                        if(--tocomplete[course] == 0) {
+                            
+                            bfs.add(course);
+                            count.getAndIncrement();
 
                         }
 
                     }));
         }
 
-        if (coursesCompletedAtomic.get() == numCourses) {
+        if (numCourses <= count.get()) {
             result = true;
         }
 
