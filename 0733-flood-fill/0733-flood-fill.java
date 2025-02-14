42703305e8dd4564c675daf043141de76fa4d8a2
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-14 12:07:42 -0800

    Time: 1 ms (46.79%), Space: 45.6 MB (10.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,13 +4,13 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         //Base case - Avoid NullPointerException
         if (image == null) return new int[0][0];
-        //Check if the rows are empty or if the sc and sc is of color
+        //Check if the rows are empty or if the sc and sc are of color
         if (image.length == 0 || image[sr][sc] == color) return image;
 
         //Initialize the directions array
         dirs = new int[][] {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-        //Calculate the length of rows and cols and take note to the current color to perform Flood fill
+        //Calculate the length of rows and cols and take note of the current color to perform Flood fill
         int startColor = image[sr][sc];
         int m = image.length;
         int n = image[0].length;
@@ -26,7 +26,7 @@ class Solution {
         //Start the Procedure of Flood Fill - change the color of the first pixel
         image[sr][sc] = color;
 
-        //Start the BFS approach - moving in four direstions UP, RIGHT, DOWN, LEFT
+        //Start the BFS approach - moving in four directions UP, RIGHT, DOWN, LEFT
         while (!rows.isEmpty()) {
             int currRow = rows.poll();
             int currCol = cols.poll();
@@ -35,7 +35,7 @@ class Solution {
                 int nr = dir[0] + currRow;
                 int nc = dir[1] + currCol;
 
-                //If you find that the new row and colums' color is the same as first pixel's color then change it
+                //If you find that the new row and column color is the same as the first pixel's color then change it
                 if (nr >= 0 && nr < m && nc >= 0 && nc < n && image[nr][nc] == startColor) {
                     image[nr][nc] = color;
                     //And record the new row and column in your BFS queue
