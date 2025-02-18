0bb43c570bd4866de53deffb9e35b52eafea0f26
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-18 15:17:05 -0800

    Time: 4 ms (51.5%), Space: 52.8 MB (7.8%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,47 @@
 class Solution {
-    int count, m, n;
-    int[][] dirs;
+    private int result;
+    private int m, n;
+    private int[][] dirs;
+
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
+        result = 0;
+
+        if (grid == null || grid.length == 0) return result;
 
         m = grid.length;
         n = grid[0].length;
-        dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        for (int i = 0; i < m; i++) {
+        dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // Right, Down, Left, Up
+
+        for (int i = 0; i < m; i ++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    count++;
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
+                    result++;
                 }
             }
         }
 
-        return count;
+        return result;
     }
 
-    private void dfs(char[][] grid, int row, int col) {
-        //Base case
-        if (row < 0 || col < 0 || row == m || col == n || grid[row][col] != '1') return;
-        //logic
-        grid[row][col] = '0';
-        for(int[] dir: dirs) {
-            int newRow = row + dir[0];
-            int newCol = col + dir[1];
-            dfs(grid, newRow, newCol);
+    private void bfs(char[][] grid, int row, int col) {
+        Queue<int[]> processIsland = new LinkedList<>();
+
+        processIsland.add(new int[] {row, col});
+
+        while (!processIsland.isEmpty()) {
+            int[] vertex = processIsland.poll();
+
+            for (int[] dir: dirs) {
+                int nr = dir[0] + vertex[0];
+                int nc = dir[1] + vertex[1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == '1') {
+                    processIsland.add(new int[] {nr, nc});
+                    grid[nr][nc] = '\u0000';                    
+                }
+            }
         }
     }
 }
