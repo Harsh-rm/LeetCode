48f4509b97b4c72943d10cb36e1203b0edc14480
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-19 13:08:50 -0800

    Time: 5 ms (9.62%), Space: 45.7 MB (5.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,33 +1,31 @@
 class Solution {
-    List<List<Integer>> result;
+    private List<List<Integer>> result;
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         result = new ArrayList<>();
+
         if (candidates == null || candidates.length == 0) return result;
 
-        helper(candidates, 0, target, new ArrayList<>());
+        helper(candidates, target, 0, new ArrayList<>());
 
         return result;
     }
 
-    private void helper(int[] candidates, int index, int target, List<Integer> path) {
-        //Base
-        if (target < 0 || index == candidates.length) {
-            return;
-        }
+    private void helper(int[] candidates, int target, int index, List<Integer> path) {
+        //Base case
         if (target == 0) {
             result.add(new ArrayList<>(path));
             return;
         }
+
+        if (index == candidates.length || target < 0) {
+            return;
+        }
         //Logic
-        //Do not choose or 0 case
-        helper(candidates, index + 1, target, path);
-        //Choose or 1 case
-        //Action
-        path.add(candidates[index]);
-        //Recurse
-        helper(candidates, index, target - candidates[index], path);
-        //Backtrack
-        path.remove(path.size() - 1);
+        helper(candidates, target, index + 1, path);
+
+        List<Integer> newPath = new ArrayList<>(path);
+        newPath.add(candidates[index]);
+        helper(candidates, target - candidates[index], index, newPath);
     }
 }
