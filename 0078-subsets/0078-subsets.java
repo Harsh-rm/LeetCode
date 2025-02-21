250e399f476c74ce9f41630ed568cb4381689209
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-20 16:46:31 -0800

    Time: 1 ms (54.43%), Space: 43 MB (30.63%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,27 @@
 class Solution {
     List<List<Integer>> result;
-
     public List<List<Integer>> subsets(int[] nums) {
         result = new ArrayList<>();
-        int n = nums.length;
-        
+
         if (nums == null || nums.length == 0) return result;
-        
-        result.add(new ArrayList<>());
-        for (int i = 0; i < n; i++) {
-            int size = result.size();
-            for(int j = 0; j < size; j++) {
-                List<Integer> newList = new ArrayList<>(result.get(j));
-                newList.add(nums[i]);
-                result.add(newList);
-            }
-        }
+
+        helper(nums, 0, new ArrayList<>());
 
         return result;
+    }
+
+    private void helper(int[] nums, int index, List<Integer> path) {
+        //Base case
+        if (index == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        //Logic
+        helper(nums, index + 1, path);
+
+        path.add(nums[index]);
+        helper(nums, index + 1, path);
+
+        path.remove(path.size() - 1);
     }
 }
