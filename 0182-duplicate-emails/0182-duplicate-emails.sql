b3e5ee0f8b9bfc8d59eeea2ffd7a7d0e735aed14
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-25 11:53:34 -0800

    Time: 699 ms (75.68%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,8 @@
 /* Write your PL/SQL query statement below */
-SELECT DISTINCT email AS Email
-FROM Person
-GROUP BY email
-HAVING COUNT(*) > 1;
+SELECT DISTINCT p1.email AS Email
+FROM Person p1 
+WHERE EXISTS (
+    SELECT 1 
+    FROM Person p2
+    WHERE p1.email = p2.email AND p1.id != p2.id
+);
