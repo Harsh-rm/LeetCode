fdb3c935ec069fbbb73d058031f759e262ed3b7b
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-25 18:12:04 -0800

    Time: 1315 ms (50%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+JOIN (SELECT e.departmentId, MAX(e.salary) AS MaxSalary
+    FROM Employee e
+    GROUP BY e.departmentId
+) MaxSalaryTable ON e.departmentId = MaxSalaryTable.departmentId AND e.salary = MaxSalaryTable.MaxSalary;
