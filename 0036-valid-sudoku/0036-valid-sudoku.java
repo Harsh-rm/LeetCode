8209bd0affc0dbc96e950584149a913e61349192
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-02-27 11:46:29 -0800

    Time: 13 ms (24.12%), Space: 45.5 MB (6.84%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,25 @@
+class Solution {
+    private boolean result;
+    public boolean isValidSudoku(char[][] board) {
+        result = false;
+
+        if (board == null || board.length == 0) return result;
+
+        Set<String> isValid = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    if (!isValid.add(c + " row " + i) || !isValid.add(c + " col " + j) || !isValid.add(c + "sub-box row" + i/3 + " col " + j/3)) {
+                        return result;
+                    }
+                }
+            }
+        }
+
+        result = true;
+
+        return result;
+    }
+}
