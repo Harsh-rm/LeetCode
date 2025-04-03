af0af7e6454c935c82c07a77cd002e08d2794319
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-04-03 11:04:41 -0700

    Time: 66 ms (20.5%), Space: 58.9 MB (43.56%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -8,15 +8,19 @@ class Solution {
 
         int n = nums.length;
 
-        pq = new PriorityQueue<>();
+        pq = new PriorityQueue<>(n, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(b, a);
+            }
+        });
 
         IntStream.range(0, n).forEach(i -> {
             pq.offer(nums[i]);
         });
 
-        for(int i = 0; i < n - k; i++) {
+        IntStream.range(0, k - 1).forEach(i -> {
             pq.remove();
-        }
+        });
 
         return pq.peek();
     }
