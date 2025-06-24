86d53c537295519fea4101f2a3b5a4a223a71f9e
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-06-23 18:49:32 -0700

    Time: 3 ms (99.69%), Space: 68.1 MB (55.48%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        slow = slow.next;
+        if (slow != null) fast = slow.next;
+
+        while (fast != null) {
+            slow.next = prev;
+            prev = slow;
+            slow = fast;
+            fast = fast.next;
+        }
+
+        if (slow != null) slow.next = prev;
+
+        fast = head;
+
+        while (slow != null) {
+        if (slow.val != fast.val) return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+}
