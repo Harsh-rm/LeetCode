a71e61c5e1fb3d14cc8e5be4662af50ed1704800
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-06-28 11:47:04 -0700

    Time: 1 ms (51.61%), Space: 42.4 MB (96.67%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -3,36 +3,34 @@ class Solution {
     private List<List<Integer>> result;
 
     public List<List<Integer>> subsets(int[] nums) {
-        
-        try{
+         
+        try {
             result = new ArrayList<>();
 
             if (nums == null || nums.length == 0) return result;
 
-            List<Integer> emptyList = new ArrayList<>();
+            helper(nums, 0, new ArrayList<>());
 
-            result.add(emptyList);
-
-            helper(nums);
-        }
-        catch (RuntimeException e) {
+            return result;
+         }
+         catch (RuntimeException e) {
             System.out.println(e.getMessage());
-        }
+         }
 
-        return result;
+         return result;
     }
 
-    private void helper(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int size = result.size();
-
-            for (int j = 0; j < size; j++) {
-                List<Integer> newList = new ArrayList<>(result.get(j));
-
-                newList.add(nums[i]);
-
-                result.add(newList);
-            }
-        }
+    private void helper(int[] nums, int index, List<Integer> path) {
+        //Capture the path at each recursive call in the result
+        result.add(new ArrayList<>(path));
+        //Using a for-loop based recursion to re-iterate later occuring indices as a 0-index
+        for (int i = index; i < nums.length; i++) {
+            //add the current element to path
+            path.add(nums[i]);
+            //recurse
+            helper(nums, i + 1, path);
+            //backtrack
+            path.remove(path.size() - 1);
+        } 
     }
 }
