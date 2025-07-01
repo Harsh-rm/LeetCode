1946cecbce9335a77269ca9c9572ef85f9c69a39
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-07-01 10:56:20 -0700

    Time: 6 ms (91.24%), Space: 42.4 MB (85.45%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,31 +1,54 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
+        if(s == null|| t == null) return true;
+        if(s.length() != t.length()) return false;
 
-        char[] sArray = new char[96]; //store sChar to tChar mapping
-        char[] tArray = new char[96]; //store tChar to sChar mapping
+        char[] sArray = new char[127];
+        char[] tArray = new char[127];
+        
+        for(int i = 0; i < s.length(); i++) {
+          char sChar = s.charAt(i);
+          char tChar = t.charAt(i);
 
-        for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-
-            if (sArray[sChar - ' '] == '\u0000') {
-                sArray[sChar - ' '] = tChar;
-            } else {
-                if (sArray[sChar - ' '] != tChar) {
-                    return false;
-                }
+          if(sArray[sChar - ' '] != '\u0000') {
+            if(sArray[sChar  - ' ']  != tChar) {
+              return false;
             }
+          }
+          else {
+             sArray[sChar - ' '] = tChar;
+          }
 
-            if (tArray[tChar - ' '] == '\u0000') {
-                tArray[tChar - ' '] = sChar;
-            } else {
-                if (tArray[tChar - ' '] != sChar) {
-                    return false;
-                }
+          if(tArray[tChar - ' '] != '\u0000') {
+            if(tArray[tChar - ' '] != sChar) {
+              return false;
             }
+          }
+          else {
+              tArray[tChar - ' '] = sChar;
+          }
         }
+        // HashMap<Character, Character> smap = new HashMap<>();
+        // // HashMap<Character, Character> tmap = new HashMap<>();
+        // HashSet<Character> tset = new HashSet<>();
 
+        // for(int i = 0; i < s.length(); i++){
+        //   char schar = s.charAt(i);
+        //   char tchar = t.charAt(i);
+          
+        //   if(smap.containsKey(schar)){
+        //     if(tchar != smap.get(schar)){
+        //       return false;
+        //     }
+        //   }
+        //   else{
+        //     if(tset.contains(tchar)){
+        //       return false;
+        //     }
+        //     smap.put(schar, tchar);
+        //     tset.add(tchar);
+        //   }
+        // }
         return true;
     }
 }
