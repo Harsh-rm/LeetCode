feab47e3e72a88bca9833bf576191d7ab76a2c14
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-07-19 14:31:24 -0700

    Time: 3 ms (100%), Space: 42.4 MB (40.13%) - LeetHub

--- 0284-peeking-iterator/0284-peeking-iterator.java
+++ 0284-peeking-iterator/0284-peeking-iterator.java
@@ -1,65 +1,64 @@
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
+import java.util.NoSuchElementException;
+
 class PeekingIterator implements Iterator<Integer> {
-    Iterator<Integer> iterator;
-    Integer peekElement;
+
+    private Iterator<Integer> iterator;
+    private Integer next;
 
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
 	    this.iterator = iterator;
-        if(this.iterator.hasNext()) {
-            peekElement = this.iterator.next();
+
+        if (this.iterator.hasNext()) {
+            next = this.iterator.next();
         }
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
         try {
-            return peekElement;
+            return next;
         }
         catch(RuntimeException e) {
-            System.err.println("Unchecked Exception at hasNext() -> class Peeking Iterator" + e);
+            System.err.println("Un-checked exception at peek() -> class PeekingIterator: " + e.getMessage());
             return null;
-        }
+        }                
 	}
 	
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-
         try {
-            Integer temp = peekElement;
+            Integer temp = next;
 
             if (iterator.hasNext()) {
-                peekElement = iterator.next();
+                next = iterator.next();
             } else {
-                peekElement = null;
+                next = null;
             }
 
             return temp;
         }
-        catch(RuntimeException e) {
-            System.err.println("Unchecked Exception at hasNext() -> class Peeking Iterator" + e);
+        catch (RuntimeException e) {
+            System.err.println("Un-checked exception in next() -> class PeekingIterator: " + e.getMessage());
             return null;
-        }        
+        }
 	}
 	
 	@Override
 	public boolean hasNext() {
-
         try {
-            if(peekElement == null) {
-                return false;
-            }
+            if (next == null) return false;
 
-            return true;
-        } 
-        catch(RuntimeException e) {
-            System.err.println("Unchecked Exception at hasNext() -> class Peeking Iterator" + e);
-            return false;
+            else return true;
         }
-
+        catch (RuntimeException e) {
+            System.err.println("Un-checked exception in hasNext() -> class PeekingIterator: " + e.getMessage());
+            return false;
+        }	    
 	}
 }
