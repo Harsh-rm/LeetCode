dddb56717b6203b1dd0add863eb8d1f9e2785a75
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-07-28 21:50:02 -0700

    Time: 83 ms (12.55%), Space: 44.4 MB (64.89%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,27 +11,18 @@
 class Solution {
 
     ListNode result;
-    int k;
+    Integer k;
 
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) return null;
 
         try {
             this.k = lists.length;
-            result = new ListNode(-1);
+            result = new ListNode(Integer.MIN_VALUE);
             ListNode curr = result;
 
-            PriorityQueue<ListNode> pq = new PriorityQueue<>(k, (a,b) -> a.val - b.val);
-
-            for (ListNode head: lists) {
-                if (head != null) pq.add(head);
-            }
-
-            while (!pq.isEmpty()) {
-                ListNode min = pq.poll();
-                curr.next = min;
-                curr = min;
-                if (min.next != null) pq.add(min.next);
+            for (ListNode li: lists) {
+                curr = merge2Lists(curr, li);
             }
 
             return result.next;
@@ -40,5 +31,32 @@ class Solution {
             System.err.println("Unchecked exception in mergeKLists -> class Solution: " + e.getMessage());
             return null;
         }
+    }
+
+    private ListNode merge2Lists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+
+        while (l1 != null & l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        }
+        if (l2 != null) {
+            curr.next = l2;
+        }
+
+        return head.next;
     }
 }
