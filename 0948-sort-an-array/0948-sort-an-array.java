a18d70d702de4f18b27e4de634a09e9ac646e9eb
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-07-29 23:44:56 -0700

    Time: 1479 ms (7.91%), Space: 55.8 MB (69.94%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,70 +1,61 @@
 class Solution {
-    private int arrayLength;
-    private boolean flag;
-
     public int[] sortArray(int[] nums) {
-        if (nums.length == 0) return nums;        
+        if (nums == null || nums.length == 0) return null;
 
-        try {
-            this.arrayLength = nums.length;
-
-            flag = heapSort(nums);
-        }
-        catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-            return new int[0]; 
-        }
-
-        if (flag) System.out.println("sortArray(nums) ran successfully!");
+        mergeSort(nums, 0, nums.length - 1);
 
         return nums;
     }
 
-    private boolean heapSort(int[] nums) {
-        int lastParent = arrayLength / 2 - 1;
+    private void mergeSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
 
-        IntStream.iterate(lastParent, i -> i >= 0, i -> i - 1)
-                .forEach(i -> {
-                    heapify(nums, i, arrayLength);
-                });
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
 
-        IntStream.iterate(arrayLength - 1, i -> i > 0, i -> i - 1)
-                .forEach(i -> {
-                    swap(nums, 0, i);
-
-                    heapify(nums, 0, i);
-                });
-
-        return true;
-    }
-
-    private void heapify(int[] nums, int index, int heapSize) {
-        int largestElementIndex = index;
-
-        int leftChild = 2 * largestElementIndex + 1;
-
-        int rightChild = 2 * largestElementIndex + 2;
-
-        if (leftChild < heapSize && nums[leftChild] > nums[largestElementIndex]) {
-            largestElementIndex = leftChild;
+            merge(nums, left, mid, right);
         }
-
-        if (rightChild < heapSize && nums[rightChild] > nums[largestElementIndex]) {
-            largestElementIndex = rightChild;
-        }
-
-        if (largestElementIndex != index) {
-            swap(nums, index, largestElementIndex);
-
-            heapify(nums, largestElementIndex, heapSize);
-        }
-    }
-
-    private void swap(int[] nums, int first, int last) {
-        int temp = nums[first];
-        nums[first] = nums[last];
-        nums[last] = temp;
 
         return;
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        int start1 = left;
+        int start2 = mid + 1;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] tempArr = new int[nums.length];
+
+        for (int i = 0; i < n1; i++) {
+            tempArr[start1 + i] = nums[start1 + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            tempArr[start2 + j] = nums[start2 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (tempArr[start1 + i] < tempArr[start2 + j]) {
+                nums[k] = tempArr[start1 + i];
+                i++;
+            }
+            else {
+                nums[k] = tempArr[start2 + j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i < n1) {
+            nums[k] = tempArr[start1 + i];
+            i++; k++;
+        }
+
+        while (j < n2) {
+            nums[k] = tempArr[start2 + j];
+            j++; k++;
+        }
     }
 }
