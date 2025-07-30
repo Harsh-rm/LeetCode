3a5160c903e8609a4dec6bfe3c832b5497d8bace
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-07-30 10:37:48 -0700

    Time: 1 ms (99.54%), Space: 56.8 MB (81.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -6,24 +6,11 @@ class Solution {
 
         int n = nums.length;
         int rSum = nums[0], max = rSum;
-        int start = 0, end = 0;
 
         for (int i = 1; i < n; i++) {
-            if (rSum + nums[i] > nums[i]) {
-                rSum += nums[i];
-            }
-            else {
-                rSum = nums[i];
-                start = i; end = i;
-            }
-
-            if (rSum > max) {
-                max = rSum;
-                end = i;
-            }
+            rSum = Math.max(rSum + nums[i], nums[i]);
+            max = Math.max(rSum, max);
         }
-
-        System.out.printf("Start: %d, End: %d", start, end);
 
         return max;
     }
