c54948db73aad9063231ea2deb7fb8c1b4655f2e
Harshith RM <98628286+Harsh-rm@users.noreply.github.com>
2025-07-30 21:56:47 -0700

    Time: 24 ms (74.68%), Space: 57.1 MB (24.42%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,61 +1,41 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        if (nums == null || nums.length == 0) return null;
-
-        mergeSort(nums, 0, nums.length - 1);
+        if (nums == null || nums.length == 0) return new int[0];
+        
+        mergeSort(nums, 0, nums.length - 1, new int[nums.length]);
 
         return nums;
     }
 
-    private void mergeSort(int[] nums, int left, int right) {
+    private void mergeSort(int[] nums, int left, int right, int[] temp) {
         if (left < right) {
             int mid = left + (right - left) / 2;
 
-            mergeSort(nums, left, mid);
-            mergeSort(nums, mid + 1, right);
+            mergeSort(nums, left, mid, temp);
+            mergeSort(nums, mid + 1, right, temp);
 
-            merge(nums, left, mid, right);
+            merge(nums, left, mid, right, temp);
         }
-
-        return;
     }
 
-    private void merge(int[] nums, int left, int mid, int right) {
-        int start1 = left;
-        int start2 = mid + 1;
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        int[] tempArr = new int[nums.length];
-
-        for (int i = 0; i < n1; i++) {
-            tempArr[start1 + i] = nums[start1 + i];
-        }
-        for (int j = 0; j < n2; j++) {
-            tempArr[start2 + j] = nums[start2 + j];
+    private void merge(int[] nums, int left, int mid, int right, int[] temp) {
+        for (int i = left; i <= right; i++) {
+            temp[i] = nums[i];
         }
 
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (tempArr[start1 + i] < tempArr[start2 + j]) {
-                nums[k] = tempArr[start1 + i];
-                i++;
+        int i = left, j = mid + 1, k = left;
+
+        while (i < mid + 1 && j <= right) {
+            if (temp[i] < temp[j]) {
+                nums[k++] = temp[i++];                
             }
             else {
-                nums[k] = tempArr[start2 + j];
-                j++;
+                nums[k++] = temp[j++];
             }
-
-            k++;
         }
 
-        while (i < n1) {
-            nums[k] = tempArr[start1 + i];
-            i++; k++;
-        }
-
-        while (j < n2) {
-            nums[k] = tempArr[start2 + j];
-            j++; k++;
+        while (i < mid + 1) {
+            nums[k++] = temp[i++];
         }
     }
 }
